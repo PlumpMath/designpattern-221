b50a474f09a0868426036386cb50f325dc38f3e9
panlover <453113032@qq.com>
2016-12-12 15:41:35 +0800

    添加装饰模式（包装模式）

---
+++ src/main/java/com/yudeyang/command/Command.java
@@ -0,0 +1,6 @@
+package com.yudeyang.command;
+
+/** The Command interface */
+public interface Command {
+   void execute();
+}
---
+++ src/main/java/com/yudeyang/command/PressSwitch.java
@@ -0,0 +1,20 @@
+package com.yudeyang.command;
+
+/* The test class or client
+*
+*
+* */
+
+public class PressSwitch {
+   public static void main(String[] args){
+
+      Light lamp = new Light();
+      Command switchUp = new FlipUpCommand(lamp);
+      Command switchDown = new FlipDownCommand(lamp);
+
+      Switch mySwitch = new Switch();
+
+      mySwitch.storeAndExecute(switchUp);
+      mySwitch.storeAndExecute(switchDown);
+   }
+}
